b7344877a3fd9e21220397015ea46a16d54dab4c
s0011x <suha.alrajhi01@gmail.com>
2023-10-08 12:40:16 +0300

    suha cimmit

---
+++ xcodeTraining1/PresentMeSUHA.swift
@@ -0,0 +1,18 @@
+//
+//  PresentMeSUHA.swift
+//  xcodeTraining1
+//
+//  Created by suha alrajhi on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct PresentMeSUHA: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PresentMeSUHA()
+}
