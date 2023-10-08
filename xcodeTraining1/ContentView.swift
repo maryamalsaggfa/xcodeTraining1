617561cf1029c2b5b98f25dd8c5d95d1feb5fc99
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-07 23:28:59 -0700

    Initial Commit

---
+++ xcodeTraining1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcodeTraining1
+//
+//  Created by maryam on 22/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcodeTraining1/xcodeTraining1App.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeTraining1App.swift
+//  xcodeTraining1
+//
+//  Created by maryam on 22/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct xcodeTraining1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
