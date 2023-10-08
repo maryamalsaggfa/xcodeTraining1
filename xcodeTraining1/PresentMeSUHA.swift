b256e397c3860b0abd3b77b8d3b923fcb70be39e
s0011x <suha.alrajhi01@gmail.com>
2023-10-08 13:03:55 +0300

    second commit

--- xcodeTraining1/PresentMeSUHA.swift
+++ xcodeTraining1/PresentMeSUHA.swift
@@ -1,18 +1,60 @@
 //
-//  PresentMeSUHA.swift
-//  xcodeTraining1
+//  ContentView.swift
+//  PresentMe
 //
 //  Created by suha alrajhi on 23/03/1445 AH.
 //
 
 import SwiftUI
+import SwiftData
 
 struct PresentMeSUHA: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 30){
+            Image("Image 2") // Replace with your image name
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.blue, lineWidth: 2))
+            
+            Text("Suha Alrajhi").font(.system(size: 30, weight: .bold, design: .serif))
+                .foregroundColor(.blue)
+            
+            Text("Hello! my name is suha and I'm 23 years old, I'm a programmer, I love music so much! and also i love going out at anytime 'call me and I'll be ready' , I see myself as a rational person but sometimes i need to be more patient ")
+                .font(.system(size: 20,design: .serif))
+            
+
+            
+            
+        }.padding()
+        
+        
+        
+        
+        
+        
     }
-}
+    }
+
 
 #Preview {
     PresentMeSUHA()
+        .modelContainer(for: Item.self, inMemory: true)
 }
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
+
